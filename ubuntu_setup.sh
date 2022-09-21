b5ac2ae8dbfd1d2ed595fe6578bfd6c311dda189
Ben <you@example.com>
2022-09-22 05:56:42 +0800

    git added

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -4,6 +4,12 @@
 # curl -LO https://raw.githubusercontent.com/code0312/linux_dev_setup/main/ubuntu_setup.sh
 
 sudo apt update && \
-sudo apt upgrade
-sudo apt install neovim snapd && \
-sudo snap install code --classic
+sudo apt upgrade && \
+sudo apt install git neovim snapd && \
+sudo snap install code --classic && \
+\
+read "Enter Git name: " gitName && \
+ git config --global user.name "$gitName" && \
+read "Enter Git email: " gitEmail && \
+ git config --global user.email "$gitEmail" && \
+ echo "Git configured for $gitName, $gitEmail"
