6209756e1eac3ccd37b831848fcb4097af5d9089
Ben <you@example.com>
2022-09-22 05:46:29 +0800

    update

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,6 +1,9 @@
 !#/bin/bash
 
 # A post-install script for Ubuntu to install developer tools
+# curl -LO https://raw.githubusercontent.com/code0312/linux_dev_setup/main/ubuntu_setup.sh
 
+sudo apt update && \
+sudo apt upgrade
 sudo apt install neovim snapd && \
 sudo snap install code --classic
