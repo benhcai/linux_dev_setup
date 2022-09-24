a4d400b088d5359ac51414f282b0dc97d5d48fe2
u <u>
2022-09-24 08:24:28 +0800

    Fixed nvm headless install

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -38,10 +38,8 @@ printC "Removing existing NVM and Node... \n" $CYAN && \
 rm -Rf ~/.npm ~/.nvm && \
 printC "Installing NVM... \n" $CYAN && \
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash && \
-source ~/.bashrc && \
-export NVM_DIR="$HOME/.nvm" && \
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && \
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" && \
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 printC "Installing node... \n" $CYAN && \
 nvm install node  && \
 \
