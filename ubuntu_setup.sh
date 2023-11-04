f1de1b05e4bc382084d7df3a5d794729d15553dd
bc <bc@bc.com>
2023-11-05 00:33:57 +1100

    fix syntax

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -9,6 +9,9 @@ curl -LO https://raw.githubusercontent.com/code0312/linux_dev_setup/main/ubuntu_
 chmod +x ubuntu_setup.sh
 '
 
+# Exit if any command fails
+set -e
+
 # Constants 
 GREEN='\033[0;32m'
 BLUE='\033[0;94m'
@@ -29,16 +32,15 @@ export LESS="$LESS -Q"
 
 ## Main apps
 
+
 printC "Updating and installing apps... \n" $CYAN && \
 sudo apt update && \
-sudo apt upgrade && \
+sudo apt upgrade 
 
 printC "Installing git... \n" $CYAN && \
-sudo apt install git
-
+sudo apt install git && \
 printC "Installing gh... \n" $CYAN && \
-sudo apt install gh
-
+sudo apt install gh && \
 printC "Installing neovim... \n" $CYAN && \
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage && \
 chmod u+x nvim.appimage && \
@@ -48,32 +50,25 @@ chmod u+x nvim.appimage && \
 sudo mv squashfs-root / && \
 sudo ln -s /squashfs-root/AppRun /usr/bin/nvim && \
 printC "Cleaning up... \n" $CYAN && \
-rm -rf FiraCode FiraCode.zip squashfs-root nvim.appimage && \
-\
+rm -rf FiraCode FiraCode.zip squashfs-root nvim.appimage
+
 printC "Removing existing NVM and Node... \n" $CYAN && \
 rm -Rf ~/.npm ~/.nvm && \
-\
 printC "Installing NVM... \n" $CYAN && \
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash && \
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" && \
 sudo [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 sudo [ -s "$NVM_DIR/nvm.sh" ] && \. ~/.nvm/nvm.sh && \
-\
 printC "Installing node... \n" $CYAN && \
 nvm install node  && \
-\
 printC "Installing yarn... \n" $CYAN && \
 npm install --global yarn && \
-\
 printC "Installed yarn version: " $GREEN && \
 yarn --version && \
-\
 printC "Installing tsc... \n" $CYAN && \
 sudo apt install node-typescript && \
-\
 printC "Installing snapd... \n" $CYAN && \
 sudo apt install snapd && \
-\
 printC "Installing vscode... \n" $CYAN && \
 sudo snap install code --classic && \
 
@@ -84,17 +79,15 @@ printC "Setting up Git... \n" $CYAN
 
 printC "Enter Git name: " $BLUE
 read gitName && \
-git config --global user.name "$gitName" && \
-
+git config --global user.name "$gitName"
 printC "Enter Git email: " $BLUE
 read gitEmail && \
-git config --global user.email "$gitEmail" && \
+git config --global user.email "$gitEmail"
+printC "Git configured for $gitName, $gitEmail \n" $GREEN
 
-printC "Git configured for $gitName, $gitEmail \n" $GREEN && \
-\
 ## Setup gh
 
 printC "Setting up gh auth... \n" $CYAN
-gh auth login && \
+gh auth login
 
 printC "Setup complete. \n" $GREEN
