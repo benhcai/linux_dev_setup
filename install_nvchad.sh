ec2bcaa61ede0362b80ab861c7a08c365c074b88
bc <bc@bc.com>
2023-11-04 23:48:26 +1100

    font install to terminal set

--- install_nvchad.sh
+++ install_nvchad.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
-#
-source ./printC.sh
 
-printC "Installing NvChad...\n" $CYAN && \
+curr_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source "${curr_dir}/printC.sh"
+
+printC "NvChad setup and install...\n" $CYAN && \
 
 printC "Installing font...\n" $BLUE && \
 curl -OL https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraCode.zip && \
+#### Install FiraCode
 unzip -d FiraCode ./FiraCode.zip && \
 mkdir -p ~/.local/share/fonts && \
 cp ./FiraCode/FiraCodeNerdFont-Regular.ttf ~/.local/share/fonts && \
 fc-cache -f && \
 fc-list | grep FiraCode && \
+#### Set FiraCode as terminal font
+PROFILE=$(gsettings get org.gnome.Terminal.ProfilesList default)
+PROFILE=${PROFILE:1:-1}
+gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$PROFILE/" font 'FiraCode Nerd Font 12'
+
 
 printC "Installing gcc...\n" $BLUE && \
 sudo apt install gcc && \
@@ -18,12 +25,16 @@ sudo apt install gcc && \
 printC "Installing make...\n" $BLUE && \
 sudo apt install make && \
 
+printC "Installing xclip (for clipboard)...\n" $BLUE && \
+sudo apt install xclip && \
+
 printC "Installing ripgrep (for telescope)...\n" $BLUE && \
 sudo apt-get install ripgrep && \
 
 printC "Removing old nvim files...\n" $BLUE && \
 rm -rf ~/.config/nvim && \
 rm -rf ~/.local/share/nvim && \
+
 printC "Cloning NvChad repo\n" $BLUE && \
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 
---
+++ pp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+dir="$(dirname "$0")"
+echo $dir
