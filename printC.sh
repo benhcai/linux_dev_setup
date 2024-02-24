e459436d0bad1dc6ee6415b18381ef2f1bddcffc
Benjamin Cai (蔡子豪) <41190525+benhcai@users.noreply.github.com>
2024-02-24 18:59:15 +1100

    Update printC.sh

--- printC.sh
+++ printC.sh
@@ -1,8 +1,9 @@
 # Constants
 # Colors https://en.wikipedia.org/wiki/ANSI_escape_code
-export GREEN='\033[0;32m'
+export GREEN='\033[0;92m'
 export BLUE='\033[0;94m'
-export CYAN='\033[0;36m'
+export CYAN='\033[0;96m'
+export RED='\033[0;91m'
 export NC='\033[0m'
 
 function printC {
